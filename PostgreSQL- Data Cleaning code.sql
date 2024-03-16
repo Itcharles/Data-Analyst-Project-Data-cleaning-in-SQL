500bf7b976a46ceb8d2fc14dfdf3cac0ec7d6627
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-03-16 13:17:59 +0100

    Create PostgreSQL- Data Cleaning code.sql

---
+++ PostgreSQL- Data Cleaning code.sql
@@ -0,0 +1,49 @@
+/*
+
+Cleaning Data in SQL Queries
+
+*/
+
+
+--Checking our data:
+
+select *
+from housing_data.housing
+where "PropertyAddress" = ''
+
+
+ --------------------------------------------------------------------------------------------------------------------------
+
+-- Populate Property Address data:
+  
+#I created 3 CTE tables where in final table 3 i have all missing addresses:
+  
+--All ParcelID where address is missing:
+with table1 AS(
+select "ParcelID", "PropertyAddress"
+from housing_data.housing
+where "PropertyAddress" = ''),
+
+--All ParcelID which has an address:
+table2 AS(
+select distinct ON ("ParcelID") "ParcelID", "PropertyAddress"
+from housing_data.housing
+where "PropertyAddress" != ''),
+
+--Joining both tables:
+table3 AS(
+select distinct a."ParcelID", b."PropertyAddress"
+from table1 a
+left join table2 b ON a."ParcelID" = b."ParcelID") 
+
+-- Updating our main table with missing addresses:
+UPDATE housing_data.housing
+SET "PropertyAddress" = Table3."PropertyAddress"
+FROM Table3
+WHERE housing_data.housing."ParcelID" = Table3."ParcelID"
+AND housing_data.housing."PropertyAddress" = ''
+
+
+ --------------------------------------------------------------------------------------------------------------------------
+
+
